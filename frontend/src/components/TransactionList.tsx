7cc159933a9b2b00552b0390d2f4d923e733afb9
Michael Klen <nitrex.code@gmail.com>
2024-08-14 00:27:28 +0200

    change tailwind styles in transaction list

--- frontend/src/components/TransactionList.tsx
+++ frontend/src/components/TransactionList.tsx
@@ -1,4 +1,3 @@
-// src/components/TransactionList.tsx
 import React from 'react';
 import { Transaction } from '../types';
 
@@ -9,11 +8,11 @@ interface TransactionListProps {
 const TransactionList: React.FC<TransactionListProps> = ({ transactions }) => {
   return (
     <div>
-      <h3 className="text-lg font-bold mt-4">Transactions</h3>
+      <h3 className="text-xl font-semibold text-gray-800 mb-4">Transakce</h3>
       <ul>
         {transactions.map((transaction) => (
-          <li key={transaction.id} className="p-2 mb-2 bg-white bg-opacity-20 rounded-lg">
-            <p>{transaction.bookingDate}: {transaction.amount.value} {transaction.amount.currency}</p>
+          <li key={transaction.id} className="p-3 mb-2 bg-gray-100 rounded-lg">
+            <p className="text-gray-600">{transaction.bookingDate}: {transaction.amount.value} {transaction.amount.currency}</p>
             <p className="text-sm text-gray-400">{transaction.details.detail1}</p>
           </li>
         ))}
